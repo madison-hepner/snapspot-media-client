30315a4171a9844a8974f5869de937785723454f
madison-hepner <hepnermadison@icloud.com>
2022-08-31 13:21:58 -0500

    slightly better, just not rendering properly

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -3,20 +3,24 @@ import { Route, Routes, Switch } from "react-router-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 import { Home } from "./Home"
 import { LocationCard } from "./components/locations/LocationCard"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
+
 
 
 export const ApplicationViews = () => {
+
     return (
         <>
-        <Switch>
-            <Route path="/" element={<div>Home</div>} />
+            <Switch>
+                <Route path="/" element={<Home />} />
 
-            <Route path="/photos" element={<div>{LocationCard}</div>} />
+                {/* <Route path="/photos" element={<div>{LocationCard}</div>} /> */}
 
                 {/* Render the animal list when http://localhost:3000/animals */}
 
 
-        </Switch>
+            </Switch>
         </>
     )
 }

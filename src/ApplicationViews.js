deecffbfd6e8309ba15d9bd94177d407e799a51a
madison-hepner <hepnermadison@icloud.com>
2022-09-08 16:21:07 -0500

    rendering still not working but some updates

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Routes, Switch } from "react-router-dom"
+import { Route, Switch, useHistory } from "react-router-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 import { Home } from "./Home"
 import { LocationCard } from "./components/locations/LocationCard"
@@ -8,19 +8,22 @@ import { Register } from "./components/auth/Register"
 
 
 
+
 export const ApplicationViews = () => {
 
     return (
         <>
-            <Switch>
-                <Route path="/" element={<Home />} />
+            <Router>
+            <Route path="/" element={
+                    <Home />
+            } />
 
                 {/* <Route path="/photos" element={<div>{LocationCard}</div>} /> */}
 
                 {/* Render the animal list when http://localhost:3000/animals */}
 
 
-            </Switch>
+            </Router>
         </>
     )
 }
---
+++ src/components/locations/LocationCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const LocationCard = () => (
+    <>
+        <h2>SnapSpot Media</h2>
+        <small>full stack capstone</small>
+
+        <address>
+            <div>more stuff here</div>
+        </address>
+    </>
+)

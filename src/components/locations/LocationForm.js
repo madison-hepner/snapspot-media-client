f5622c97b32551a2f2e9ef3ccb168363e6f9cfe1
madison-hepner <hepnermadison@icloud.com>
2022-09-21 14:41:09 -0500

    style updates

--- src/components/locations/LocationForm.js
+++ src/components/locations/LocationForm.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react"
 import { useHistory } from 'react-router-dom'
 import { createLocationPost, getLocationTypes, getLocationPostById, getLocations } from "./LocationManager"
+import "./LocationForm.css"
 
 
 export const LocationPostForm = () => {
